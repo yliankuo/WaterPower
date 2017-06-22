1d3e8cef8f86291e608d6f939a30b50270ccce7e
BuildTools <unconfigured@null.spigotmc.org>
2017-06-21 22:20:48 -0400

    Fixed LightHouse no power light (maybe?)

---
+++ WaterPower/Source/WaterPower/UpdateNews.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Verse;
+using System.Collections.Generic;
+namespace RimWorld
+{
+	public class IncidentWorker_WaterPowerUpdate : IncidentWorker
+	{
+		public override bool TryExecute(IncidentParms parms){
+			Map map = (Map)parms.target;
+			Find.LetterStack.ReceiveLetter("LetterLabelWaterPowerUpdate".Translate(),"WaterPowerUpdate".Translate(new object[0]), LetterDefOf.Good,null);
+			Find.TickManager.slower.SignalForceNormalSpeedShort();
+			return true;
+		}
+	}
+}
